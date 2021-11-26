48201f87690152a40073893df0cbf80f4a3d5e04
ldestenay <B00148802@mytudublin.ie>
2021-11-26 15:38:06 +0000

    add create tables to the export

--- database.sql
+++ database.sql
@@ -12,6 +12,20 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Listage de la structure de la base pour rich_web_application
+CREATE DATABASE IF NOT EXISTS `rich_web_application` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `rich_web_application`;
+
+-- Listage de la structure de la table rich_web_application. login
+CREATE TABLE IF NOT EXISTS `login` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `acctype` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
 -- Listage des données de la table rich_web_application.login : ~5 rows (environ)
 /*!40000 ALTER TABLE `login` DISABLE KEYS */;
 INSERT INTO `login` (`id`, `username`, `password`, `acctype`) VALUES
@@ -22,6 +36,16 @@ INSERT INTO `login` (`id`, `username`, `password`, `acctype`) VALUES
 	(8, 'KJH', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'customer');
 /*!40000 ALTER TABLE `login` ENABLE KEYS */;
 
+-- Listage de la structure de la table rich_web_application. order
+CREATE TABLE IF NOT EXISTS `order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_content` int(11) NOT NULL DEFAULT 0,
+  `quantity` int(2) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_Order_Product` (`order_content`),
+  CONSTRAINT `FK_Order_Product` FOREIGN KEY (`order_content`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
 -- Listage des données de la table rich_web_application.order : ~4 rows (environ)
 /*!40000 ALTER TABLE `order` DISABLE KEYS */;
 INSERT INTO `order` (`id`, `order_content`, `quantity`) VALUES
@@ -30,6 +54,15 @@ INSERT INTO `order` (`id`, `order_content`, `quantity`) VALUES
 	(10, 5, 1),
 	(11, 4, 2);
 /*!40000 ALTER TABLE `order` ENABLE KEYS */;
+
+-- Listage de la structure de la table rich_web_application. product
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pname` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `qty` int(11) DEFAULT 0,
+  `price` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Listage des données de la table rich_web_application.product : ~5 rows (environ)
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
